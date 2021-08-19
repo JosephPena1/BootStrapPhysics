2ec12072205201f7d6423e44d4d77b394de019b5
Joseph Peña <jgpena2888@gmail.com>
2021-08-19 08:49:01 -0500

    Added missing code from monday;

--- PhysicsProject/PhysicsGame.cpp
+++ PhysicsProject/PhysicsGame.cpp
@@ -7,27 +7,38 @@
 bool PhysicsGame::startup()
 {
 	m_renderer = new aie::Renderer2D();
-	setBackgroundColour(0.2f, 0.1f, 0.0f, 1.0f);
+	setBackgroundColour(0.2f, 0.2f, 0.6f, 1.0f);
 
-	m_font = new aie::Font("../bin/font/consolas.tts");
+	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
 
 	return true;
 }
 
 void PhysicsGame::shutdown()
 {
-	delete m_renderer;
 	delete m_font;
+	delete m_renderer;
 }
 
 void PhysicsGame::update(float deltaTime)
 {
+	//Get the input instance
 	aie::Input* input = aie::Input::getInstance();
 
-
+	//Exit on Esc
+	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
+		quit();
 }
 
 void PhysicsGame::draw()
 {
+	clearScreen();
+	m_renderer->begin();
 
+	//Draw the fps
+	char fps[32];
+	sprintf_s(fps, 32, "FPS: %i", getFPS());
+	m_renderer->setRenderColour(1, 1, 1, 1);
+	m_renderer->drawText(m_font, fps, 0.0f, 0.0f);
+	m_renderer->end();
 }

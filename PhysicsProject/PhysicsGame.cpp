aac4eb1e7dae81e63cd993068a73371410b5d625
Joseph Peña <jgpena2888@gmail.com>
2021-08-24 15:53:30 -0500

    Made plane walls;

--- PhysicsProject/PhysicsGame.cpp
+++ PhysicsProject/PhysicsGame.cpp
@@ -17,17 +17,38 @@ bool PhysicsGame::startup()
 
 	m_scene = new PhysicsScene();
 	m_scene->setTimeStep(0.01f);
-	m_scene->setGravity({0.0f, -10.0f});
+	m_scene->setGravity({0.0f, 0.0f});
 
-	Sphere* ball = new Sphere(glm::vec2(-20, 0), glm::vec2(), 1, 5, glm::vec4(0.8f, 0.2f, 0.2f, 0.8f));
-	m_scene->addActor(ball);
-	ball->applyForce(glm::vec2(20.0f, 0.0f));
 
-	Sphere* ball2 = new Sphere(glm::vec2(20, 0), glm::vec2(), 1, 5, glm::vec4(0.2f, 0.8f, 0.2f, 0.8f));
+	Sphere* cue = new Sphere(glm::vec2(-20, 0), glm::vec2(), 5, 5, glm::vec4(0.8f, 0.2f, 0.2f, 0.8f));
+	m_scene->addActor(cue);
+	cue->applyForce(glm::vec2(100.0f, 0.0f));
+
+	Sphere* ball1 = new Sphere(glm::vec2(20, 0), glm::vec2(), 1, 5, glm::vec4(0.2f, 0.2f, 0.2f, 0.8f));
+	m_scene->addActor(ball1);
+	Sphere* ball2 = new Sphere(glm::vec2(30, 7), glm::vec2(), 1, 5, glm::vec4(0.4f, 0.9f, 0.7f, 0.8f));
 	m_scene->addActor(ball2);
+	Sphere* ball3 = new Sphere(glm::vec2(30, -7), glm::vec2(), 1, 5, glm::vec4(0.6f, 0.0f, 0.5f, 0.8f));
+	m_scene->addActor(ball3);
+	Sphere* ball4 = new Sphere(glm::vec2(40, 14), glm::vec2(), 1, 5, glm::vec4(0.3f, 0.1f, 0.2f, 0.8f));
+	m_scene->addActor(ball4);
+	Sphere* ball5 = new Sphere(glm::vec2(40, -14), glm::vec2(), 1, 5, glm::vec4(0.2f, 0.5f, 0.2f, 0.8f));
+	m_scene->addActor(ball5);
+	Sphere* ball6 = new Sphere(glm::vec2(40, 0), glm::vec2(), 1, 5, glm::vec4(0.2f, 0.3f, 0.6f, 0.4f));
+	m_scene->addActor(ball6);
 
-	Plane* plane = new Plane(glm::vec2(0.2f, 1.0f), -30.0f, glm::vec4(0.2f, 0.8f, 0.2f, 0.8f));
-	m_scene->addActor(plane);
+	//Boundary
+	Plane* top = new Plane(glm::vec2(0.0f, -1.0f), -55.0f, glm::vec4(0.2f, 0.8f, 0.2f, 0.8f));
+	m_scene->addActor(top);
+
+	Plane* bottom = new Plane(glm::vec2(0.0f, 1.0f), -55.0f, glm::vec4(0.2f, 0.8f, 0.2f, 0.8f));
+	m_scene->addActor(bottom);
+
+	Plane* left = new Plane(glm::vec2(1.0f, 0.0f), -95.0f, glm::vec4(0.2f, 0.8f, 0.2f, 0.8f));
+	m_scene->addActor(left);
+
+	Plane* right = new Plane(glm::vec2(-1.0f, 0.0f), -95.0f, glm::vec4(0.2f, 0.8f, 0.2f, 0.8f));
+	m_scene->addActor(right);
 
 	return true;
 }
@@ -52,6 +73,14 @@ void PhysicsGame::update(float deltaTime)
 	//Exit on Esc
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
+	if (input->isKeyDown(aie::INPUT_KEY_UP))
+		m_scene->setGravity({ m_scene->getGravity().x, m_scene->getGravity().y + 10});
+	if (input->isKeyDown(aie::INPUT_KEY_DOWN))
+		m_scene->setGravity({ m_scene->getGravity().x, m_scene->getGravity().y - 10 });
+	if (input->isKeyDown(aie::INPUT_KEY_LEFT))
+		m_scene->setGravity({ m_scene->getGravity().x - 10, m_scene->getGravity().y});
+	if (input->isKeyDown(aie::INPUT_KEY_RIGHT))
+		m_scene->setGravity({ m_scene->getGravity().x + 10, m_scene->getGravity().y});
 }
 
 void PhysicsGame::draw()

3b474af9ef216ff986bb9d153cef34d56fdab95b
Joseph Peña <jgpena2888@gmail.com>
2021-08-24 16:54:10 -0500

    Added improved collision resolution to SphereToPlane;

--- PhysicsProject/Plane.cpp
+++ PhysicsProject/Plane.cpp
@@ -1,6 +1,7 @@
 #include "Plane.h"
 #include "Gizmos.h"
 #include <glm/ext.hpp>
+#include "RigidBody.h"
 
 Plane::Plane(glm::vec2 normal, float distance, glm::vec4 color) : PhysicsObject(ShapeType::PLANE)
 {
@@ -29,4 +30,29 @@ void Plane::draw()
 	glm::vec2 end = centerPoint - (parallel * lineSegmentLength);
 
 	aie::Gizmos::add2DLine(start, end, m_color);
+}
+
+void Plane::resolveCollision(RigidBody* other)
+{
+	//j = ((-(1 + e) * Vrel) dot n) / (n dot (n * (1 / MA + 1 / MB)))
+	//j: Impulse magnitude
+	//e: Coefficient of elasticity
+	//Vrel: Relative velocity before the collision
+	//n: Collision normal
+	//M: Mass of object
+
+	//e
+	float elasticity = 1.0f;
+	//Vrel
+	glm::vec2 relativeVelocity = other->getVelocity() - 0.0f;
+	//n
+	glm::vec2 collisionNormal = getNormal();
+	//MA
+	float mass = other->getMass();
+
+	//j
+	float impulse = glm::dot((-(1 + elasticity) * relativeVelocity), collisionNormal)
+		/ glm::dot(collisionNormal, (collisionNormal * (1 / mass)));
+
+	other->applyForce(collisionNormal * impulse);
 }

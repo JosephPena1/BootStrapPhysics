fe3dbede15b311687a25aac8e78a3d865377b330
Joseph Peña <jgpena2888@gmail.com>
2021-08-26 08:41:56 -0500

    Fixed Plane.cpp;

--- PhysicsProject/Plane.cpp
+++ PhysicsProject/Plane.cpp
@@ -25,7 +25,7 @@ void Plane::draw()
 	float lineSegmentLength = 300.0f;
 	glm::vec2 centerPoint = m_normal * m_distance;
 	glm::vec2 parallel(m_normal.y, -m_normal.x);
-	
+
 	glm::vec2 start = centerPoint + (parallel * lineSegmentLength);
 	glm::vec2 end = centerPoint - (parallel * lineSegmentLength);
 

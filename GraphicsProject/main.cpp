2a1ee67011b82dfad6283d8e7d37b489edfb598a
Joseph Peña <jgpena2888@gmail.com>
2021-09-28 10:56:58 -0500

    Created GraphicsProject;

---
+++ GraphicsProject/main.cpp
@@ -0,0 +1,18 @@
+#include "gl_core_4_4.h"
+#include "GLFW/glfw3.h"
+
+int main()
+{
+	if (!glfwInit())
+		return -1;
+
+	/*GLFWwindow window = new GLFWwindow();
+
+	glfwSetWindowSize();
+
+	glfwCreateWindow();*/
+
+	glfwTerminate();
+
+	return 0;
+}

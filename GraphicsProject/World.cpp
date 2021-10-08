6cf294e24c0ec5548f2710fb5b748ae030d9d617
Joseph Peña <jgpena2888@gmail.com>
2021-10-08 15:33:18 -0500

    Changed the camera to support movement;

--- GraphicsProject/World.cpp
+++ GraphicsProject/World.cpp
@@ -14,11 +14,14 @@ void World::start()
 	m_quad.start();
 
 	//Create camera transforms
-	m_camera.setTransform(glm::lookAt(
-		glm::vec3(5, 5, 5),
+	/*m_camera.setTransform(glm::lookAt(
+		glm::vec3(1.0f, 1.0f, 1.0f),
 		glm::vec3(0),
 		glm::vec3(0, 1, 0))
-	);
+	);*/
+	m_camera.setPosition(glm::vec3(2.0f, 2.0f, 2.0f));
+	m_camera.setYaw(-135);
+	m_camera.setPitch(-40);
 
 	m_projectionMatrix = glm::perspective(
 		m_camera.getFieldOfView() * glm::pi<float>() / 180.0f,
@@ -45,5 +48,5 @@ void World::end()
 
 glm::mat4 World::getProjectionViewModel()
 {
-	return m_projectionMatrix * m_camera.getTransform() * m_quad.getTransform();
+	return m_projectionMatrix * m_camera.getViewMatrix() * m_quad.getTransform();
 }
